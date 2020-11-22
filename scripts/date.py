43efe6aa18bd59651fef39d24ace51b4eec237db
ksyasuda <ksyasuda@umich.edu>
2020-11-21 19:30:09 -0500

    Change steam to always float and edit the jdate module to print japanese year also

--- scripts/date.py
+++ scripts/date.py
@@ -1,6 +1,63 @@
 #!/usr/bin/env python
 
 from datetime import date
+    
+digits = ['一', '二', '三', '四', '五', '六', '七', '八', '九']
+
+def get_thousands(num):
+    if num == 0:
+        return '' 
+    sen = '千'
+    j_str = ''
+    first = num // 1000
+    # for 1000, you don't need to say 1
+    if first > 1:
+        j_str += digits[first - 1]
+        return j_str + sen 
+    return sen 
+
+def get_hundreds(num):
+    if num == 0:
+        return '' 
+    j_str = ''
+    hyaku = '百'
+    first = num // 100
+    if first > 1:
+        j_str += digits[first - 1]
+        return j_str + hyaku 
+    return hyaku 
+
+def get_tens(num):
+    if num == 0:
+        return '' 
+    j_str = ''
+    juu = '十'
+    first = num // 10
+    if first > 1:
+        j_str += digits[first - 1]
+        return j_str + juu
+    return juu
+
+def get_ones(num):
+    if num == 0:
+        return '' 
+    return digits[num - 1]
+
+
+def parse_year(year):
+    multipliers = [1000, 100, 10, 1]
+    places = []
+    count = 0
+    # assumes that we won't ever make it to year 10,000
+    for number in str(year):
+       places.append(int(number) * multipliers[count]) 
+       count += 1
+    j_thousands = get_thousands(places[0])
+    j_hundreds = get_hundreds(places[1])
+    j_tens = get_tens(places[2])
+    j_ones = get_ones(places[3])
+    j_year = j_thousands + j_hundreds + j_tens + j_ones + '年'
+    return j_year
 
 # get the current date
 today = date.today()
@@ -16,4 +73,7 @@ day_arr = ['一日', 'ニ日', '三日', '四日', '五日', '六日', '七日',
 
 j_month = month_arr[month - 1]
 j_day = day_arr[day - 1]
-print(j_month+j_day+str(year)+'年')
+j_year = parse_year(year)
+# with year as a stirng of numbers istead of japanese characters
+# print(j_month+j_day+str(year)+'年')
+print(j_month+j_day+j_year)
