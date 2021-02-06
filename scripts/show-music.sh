88fae57e4ebcde419a6df727aa2799bbde58a095
ksyasuda <ksyasuda@umich.edu>
2021-02-06 03:59:05 -0500

    fix icon not being defined
    also fixed icon still showing when mpc stopped

--- scripts/show-music.sh
+++ scripts/show-music.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
-
 ## depreciated use zscroll now
 
-# checkRunning=$(mpc current -f '%title% - %artist%' | cut -c -55)
-# if [[ $checkRunning == '' ]]; then
-# 	exit 1
-# fi
+checkRunning=$(mpc current)
+if [[ $checkRunning == '' ]]; then
+	exit 1
+fi
 # 
 # checkPlaying=$(mpc status | grep playing)
 # icon=''
@@ -22,7 +20,7 @@ set -Eeuo pipefail
 # "玲"
 # "怜"
 
-mpc current | zscroll --before-text "$icon" --delay 0.3 \
+mpc current | zscroll --delay 0.3 \
 		--match-command "mpc status" \
 		--match-text "playing" "--before-text ' '" \
 		--match-text "paused" "--before-text '契 ' --scroll 0" \
